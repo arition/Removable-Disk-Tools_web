47a384e8765a0a237e611f0dc6478387eaecbfa5
arition <aritionkb@gmail.com>
2014-05-07 19:02:34 +0800

    simple device page

---
+++ device.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>设备列表 - Removable Disk Tools</title>
+    <!-- Sets initial viewport load and disables zooming  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="css/bootflat.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- site css -->
+    <link rel="stylesheet" href="css/site.css">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="http://cdn.bootcss.com/html5shiv/3.7.0/html5shiv.min.js"></script>
+      <script src="http://cdn.bootcss.com/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Removable Disk Tools</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a class="nav-link" href="#">设备管理</a></li>
+          <li><a class="nav-link" href="#">文件列表</a></li>
+          <li><a class="nav-link" href="#">关于</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="starter-template"> 
+        <h3>设备列表  <small>Device List</small></h3>
+      </div>
+    </div>
+  	<script src="js/jquery.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
+  </body>
+ </html>
